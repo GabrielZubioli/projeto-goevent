b706a0346288957e59b9263ca56581b6e3bea350
Gabriel Zubioli <zubioligabriel@gmail.com>
2025-08-21 23:07:57 -0300

    Create Route

--- goevent/routes/web.php
+++ goevent/routes/web.php
@@ -1,9 +1,19 @@
 <?php
-use App\Http\Controllers\LoginController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
 
-Route::get('/login',[LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
 
-Route::fallback(function(){
-    return "Erro ao localizar a rota!";
-});
+Route::get('/register', [RegisterController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+
+Route::get('/forgot-password', [ForgotPasswordController::class, 'showForgotForm'])->name('password.request');
+Route::post('/forgot-password', [ForgotPasswordController::class, 'updatePassword'])->name('password.update');
+
+
+// Dashboard protegido
+Route::get('/dashboard', function () {
+    return 'Bem-vindo ao dashboard!';
+})->middleware('auth')->name('dashboard');
