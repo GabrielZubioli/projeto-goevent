679b32dd9dbc1903c222b2a791ef732043c7b59d
Gabriel Zubioli <zubioligabriel@gmail.com>
2025-08-24 10:10:31 -0300

    create routes

--- goevent/routes/api.php
+++ goevent/routes/api.php
@@ -2,17 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/events', [EventController::class, 'index']);
+    Route::post('/events', [EventController::class, 'store']);
+    Route::post('/events/{id}/interested', [EventController::class, 'interested']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
--- goevent/routes/web.php
+++ goevent/routes/web.php
@@ -1,8 +1,13 @@
 <?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 
+// Auth
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('login.store');
 
@@ -11,6 +16,20 @@ Route::post('/register', [RegisterController::class, 'store'])->name('register.s
 
 Route::get('/forgot-password', [ForgotPasswordController::class, 'showForgotForm'])->name('password.request');
 Route::post('/forgot-password', [ForgotPasswordController::class, 'updatePassword'])->name('password.update');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/events', fn() => view('home.events'))->name('events');
+Route::get('/profile', [ProfileController::class, 'showContent'])->name('profile');
+Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/events-data', [EventController::class, 'index'])->name('events.data');
+    Route::post('/events', [EventController::class, 'store'])->name('events.store');
+    Route::post('/events/{id}/interested', [EventController::class, 'interested'])->name('events.interested');
+});
+
 
 
 // Dashboard protegido
