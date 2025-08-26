916b1758f9441b663446a83109e59fec9b94cde9
Gabriel Zubioli <zubioligabriel@gmail.com>
2025-08-25 22:00:28 -0300

    Adjusting routes

--- goevent/routes/web.php
+++ goevent/routes/web.php
@@ -1,15 +1,22 @@
 <?php
+
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 
-// Auth
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+
+Route::post('/logout', function () {
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+    return redirect('/login');
+})->name('logout');
 
 Route::get('/register', [RegisterController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
@@ -17,22 +24,16 @@ Route::post('/register', [RegisterController::class, 'store'])->name('register.s
 Route::get('/forgot-password', [ForgotPasswordController::class, 'showForgotForm'])->name('password.request');
 Route::post('/forgot-password', [ForgotPasswordController::class, 'updatePassword'])->name('password.update');
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-Route::get('/events', fn() => view('home.events'))->name('events');
-Route::get('/profile', [ProfileController::class, 'showContent'])->name('profile');
-Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
-
-
 Route::middleware('auth')->group(function () {
+    Route::get('/events', fn() => view('events'))->name('events');
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+
     Route::get('/events-data', [EventController::class, 'index'])->name('events.data');
     Route::post('/events', [EventController::class, 'store'])->name('events.store');
+    Route::put('/events/{id}', [EventController::class, 'update'])->name('events.update');
+    Route::delete('/events/{id}', [EventController::class, 'destroy'])->name('events.destroy');
     Route::post('/events/{id}/interested', [EventController::class, 'interested'])->name('events.interested');
+
+    Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+    Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password');
 });
-
-
-
-// Dashboard protegido
-Route::get('/dashboard', function () {
-    return 'Bem-vindo ao dashboard!';
-})->middleware('auth')->name('dashboard');
